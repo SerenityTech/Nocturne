b14068f5c5898d56c2243e40f169ba2a13cef897
vctv <Serenity-Tech@outlook.com>
2024-08-09 11:39:47 +0800

    feat:✨ add a case.

--- jsvmp-yrx-2-3/deconfuse.js
+++ jsvmp-yrx-2-3/deconfuse.js
@@ -18,281 +18,287 @@ decodeFile = "./decode.js";
 let sourceCode = fs.readFileSync(encodeFile, {encoding: "utf-8"});
 let ast = parser.parse(sourceCode);
 
+
 const IfToCase = {
-    IfStatement: {
-        exit(path, state) {  // 对state参数有疑问请看文档 https://alanhays.github.io/posts/35002.html#pathtraversestate
-            const {test, consequent, alternate} = path.node;
-            let {name, cases} = state;
-            // 如果test不是 o < 数字，则不进行处理
-            if (!(types.isBinaryExpression(test, {operator: "<"}))) return;
-            if (!types.isIdentifier(test.left, {name: name})) return;
-            if (!types.isNumericLiteral(test.right)) return;
+  IfStatement: {
+    exit(path, state) {
+      const { test, consequent, alternate } = path.node;
+      let { name, cases } = state;
+      // 如果 test 不是 o < 数字，则不进行处理
+      if (!types.isBinaryExpression(test, { operator: "<" })) return;
+      if (!types.isIdentifier(test.left, { name: name })) return;
+      if (!types.isNumericLiteral(test.right)) return;
 
-            alternate.body.push(types.BreakStatement());
-            consequent.body.push(types.BreakStatement());
+      // alternate.body.push(types.BreakStatement());
+      // consequent.body.push(types.BreakStatement());
 
-            const right = test.right.value;
+      const right = test.right.value;
 
-            if (right % 2 === 0) {
-                //处理else不是if节点的情况
-                if (types.isIfStatement(alternate.body[0])) return;
-                cases.push(types.SwitchCase(types.valueToNode(right), alternate.body));
-                return;
-            }
+      if (right % 2 === 0) {
+        // 处理 else 不是 if 节点的情况
+        if (types.isIfStatement(alternate.body[0])) return;
+        cases.push(types.SwitchCase(types.valueToNode(right), alternate.body));
+        return;
+      }
 
-            // 构建case节点
-            const case1 = types.SwitchCase(types.valueToNode(right - 1), consequent.body);
-            const case2 = types.SwitchCase(types.valueToNode(right), alternate.body);
-            cases.push(case1, case2);
-        }
-    }
-}
-
-
-const IfToSwitch = {
-    ForStatement(path) {
-        const {init, test, update, body} = path.node;
-        const prev = path.getPrevSibling();
-
-        if (!types.isIfStatement(body)) return;
-
-        const discriminant = prev.node.declarations[0].id;
-
-        let cases = [];
-
-        path.traverse(IfToCase, {name: discriminant.name, cases: cases});
-
-        if (!cases.length) return;
-
-        const switchNode = types.SwitchStatement(discriminant, cases);
-
-        path.get('body').replaceInline(switchNode);
-    }
+      // 构建 case 节点
+      const case1 = types.SwitchCase(
+        types.valueToNode(right - 1),
+        consequent.body
+      );
+      const case2 = types.SwitchCase(types.valueToNode(right), alternate.body);
+      cases.push(case1, case2);
+    },
+  },
 };
 
+const IfToSwitch = {
+  ForStatement(path) {
+    const { init, test, update, body } = path.node;
+    const prev = path.getPrevSibling();
+
+    if (!types.isIfStatement(body)) return;
+
+    const discriminant = prev.node.declarations[0].id;
+
+    let cases = [];
+
+    path.traverse(IfToCase, { name: discriminant.name, cases: cases });
+
+    if (!cases.length) return;
+
+    const switchNode = types.SwitchStatement(discriminant, cases);
+
+    path.get("body").replaceInline(switchNode);
+  },
+};
+
+
 const RestoreJump = {
-    AssignmentExpression(path) {
-        let {operator, left, right} = path.node;
+  AssignmentExpression(path) {
+    let { operator, left, right } = path.node;
 
-        if (!(operator[1] == "=" &&
-            types.isNumericLiteral(right))) return;
+    if (!(operator[1] === "=" && types.isNumericLiteral(right))) return;
 
-        let caseNode = path.findParent((p) => p.isSwitchCase());
+    let caseNode = path.findParent((p) => p.isSwitchCase());
 
-        let {consequent, test} = caseNode.node;
-        // 构建 ast 节点  o -= xxx  改为 o - xxx ,
+    let { consequent, test } = caseNode.node;
+    // 构建 ast 节点  o -= xxx  改为 o - xxx ,
 
-        let _node = types.AssignmentExpression("=", left, types.valueToNode(
-            operator[0] == "-" ? test.value - right.value : test.value + right.value
-        ))
+    let _node = types.assignmentExpression(
+      "=",
+      left,
+      types.valueToNode(
+        operator[0] === "-"
+          ? test.value - right.value
+          : test.value + right.value
+      )
+    );
+    path.replaceInline(_node);
+  },
+};
 
-        path.replaceInline(_node)
-    }
-}
-
-let stack = [];
-
-function getBody(cases, index, stack, body, name) {
-
-    stack.push(index)
-    let {consequent} = cases[index];
-    let node = consequent[consequent.length - 2];
-    if (!body) {
-        body = body.concat(consequent)
-    }
-    if (!getJumpIndex(node, name) == undefined)
-        return body;
-
-    if (types.isIfStatement(node)) {
-        let {test, consequent, alternate} = node;
-        return body
-    }
-
-    if (types.isExpressionStatement(node)) {
-        index = node.expression.right.value;
-        body = body.concat(consequent.slice(0, -2))
-        return getBody(cases, index, stack, body, name)
-    }
-    // console.log(generator(types.BlockStatement(body)).code)
-    return body
-}
 
 function getJump(node, name) {
-    if (!types.isExpressionStatement(node)) return;
-    let {expression} = node;
-    if (!types.isAssignmentExpression(expression)) return;
-    if (expression.left.name != name) return;
-    return expression.right.value;
+  if (!types.isExpressionStatement(node)) return;
+  let { expression } = node;
+  if (!types.isAssignmentExpression(expression)) return;
+  if (expression.left.name !== name) return;
+  return expression.right.value;
 }
 
-function getJumpIndex(node, name) {
-    if (!types.isIfStatement(node)) return getJump(node, name);
-    let {consequent, alternate} = node;
-    if (getJump(consequent, name) == undefined || getJump(alternate, name) == undefined) return;
-    return -1;
-}
-
-const BlockSyntax = {
-    "ForStatement|WhileStatement|ForInStatement|ForOfStatement"({node}) {
-        if (!types.isBlockStatement(node.body)) {
-            node.body = types.BlockStatement([node.body]);
-        }
-    },
-    IfStatement(path) {
-        let nodes = ["consequent", "alternate"];
-        for (let i = 0; i < nodes.length; i++) {
-            let _path = path.get(nodes[i]);
-            if (_path.node && !_path.isBlockStatement()) {
-                _path.replaceInline(types.BlockStatement([_path.node]))
-            }
-        }
+function getCaseJump(map, name, node) {
+  let { test, consequent } = node;
+  let list = [];
+  let con = consequent[consequent.length - 1];
+  let num = getJump(con, name);
+  if (num !== undefined) {
+    list.push(num);
+  }
+  if (types.isIfStatement(con)) {
+    let { consequent, alternate } = con;
+    let num = getJump(consequent, name);
+    if (num !== undefined) {
+      list.push(num);
     }
-}
-
-function getNextValue(node) {
-    let {expression} = node[node.length - 1];
-    if (!types.isAssignmentExpression(expression)) return -1;
-    return expression.right.value;
-}
-
-const MergeCasesAll = {
-    ForStatement(path) {
-        let {init, test, update, body} = path.node;
-        const prev = path.getPrevSibling();
-        if (!types.isSwitchStatement(body)) return;
-        let {declarations} = prev.node;
-        const test_name = declarations[0].id.name;
-        const start = declarations[0].init.right.value;
-        path.traverse({
-            SwitchStatement(_path) {
-                let {discriminant} = _path.node;
-                if (test_name != discriminant.name) return;
-                _path.traverse({
-                    SwitchCase(__path) {
-                        let {test} = __path.node;
-                        if (test.value != start) return;
-                        __path.traverse(MergeCasesIf)
-                    }
-                })
-            }
-        })
-        path.get('body').replaceInline(types.BlockStatement(body.cases[start].consequent))
+    num = getJump(alternate, name);
+    if (num !== undefined) {
+      list.push(num);
     }
+  }
+  map[test.value] = list;
 }
 
-const MergeJump = {
-    SwitchCase(path) {
-        let {cases, discriminant} = path.parent;
-        let {test, consequent} = path.node;
-        let {expression} = consequent[consequent.length - 2];
+function removeDuplicates(arr1, arr2) {
+  let result = []; // 存储删除的元素的数组
 
-        if (!types.isAssignmentExpression(expression)) return;
+  for (let i = 0; i < arr1.length; i++) {
+    let found = false;
 
-        if (expression.left.name != discriminant.name) return;
-
-        let next = expression.right.value;
-
-        consequent.pop()
-        consequent.pop()
-
-        consequent = consequent.concat(cases[next].consequent)
-        let caseNode = types.SwitchCase(test, consequent)
-        path.replaceInline(caseNode)
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        result.push(arr1[i]); // 将相同的元素存储到结果数组中
+        arr2.splice(j, 1); // 删除arr2中的相同元素
+        found = true;
+        break;
+      }
     }
+    if (found) {
+      arr1.splice(i, 1); // 删除arr1中的相同元素
+      i--; // 由于删除了元素，需要调整索引
+    }
+  }
+  return result;
+}
+// 统一还原控制流
+function controlFlowStructure(si, map, cases, stack = [], body = []) {
+  if (!map.loop) map.loop = [];
+  if (stack.includes(si)) {
+    if (map.loop.indexOf(si) === -1) map.loop.push(si);
+    return body;
+  }
+  let item = map[si];
+  body = body.concat(cases[si].consequent);
+  switch (item.length) {
+    case 0:
+      return body;
+    case 1:
+      return controlFlowStructure(item[0], map, cases, stack, body);
+    case 2:
+      stack.push(si);
+      body[body.length - 1].consequent = types.blockStatement(
+        controlFlowStructure(item[0], map, cases, stack, [])
+      );
+      if (map.loop.includes(si)) {
+        let { test, consequent } = body[body.length - 1];
+        body[body.length - 1] = types.whileStatement(test, consequent);
+        body = body.concat(
+          controlFlowStructure(item[1], map, cases, stack, [])
+        );
+      } else {
+        body[body.length - 1].alternate = types.blockStatement(
+          controlFlowStructure(item[1], map, cases, stack, [])
+        );
+        body = body.concat(
+          removeDuplicates(
+            body[body.length - 1].consequent.body,
+            body[body.length - 1].alternate.body
+          )
+        );
+      }
+      stack.pop();
+      return body;
+  }
 }
 
-const MergeDeadCycle = {
-    SwitchCase(path) {
-        let {discriminant} = path.parent;
-        let {test, consequent} = path.node;
-        let index = consequent.length - 2;
-        let node = consequent[index];
-        let next = getJumpIndex(node, discriminant.name);
-        if (next == undefined) return;
-        if (next != -1) return;
-        let {alternate} = node;
-        let n1 = getJump(node.consequent, discriminant.name)
-        if (n1 != test.value) return;
-        n1 = consequent.pop()
-        consequent = consequent.slice(0, consequent.length - 1)
-        node = types.whileStatement(node.test, types.BlockStatement(consequent))
-        path.replaceInline(types.SwitchCase(test, [node, alternate, n1]))
+const MergeCases = {
+  SwitchStatement(path) {
+    const { discriminant, cases } = path.node;
+    const name = discriminant.name;
+    let binding = path.scope.getBinding(name);
+    let start = binding.path.node.init.right.value;
+    let map = {};
+    for (let i = 0; i < cases.length; i++) {
+      getCaseJump(map, name, cases[i]);
     }
+    path.replaceInline(controlFlowStructure(start, map, cases));
+  },
+};
+
+
+const CleaningUpGarbage = {
+  ForStatement(path) {
+    let p = path.getPrevSibling();
+    let name = p.node.declarations[0].id.name;
+    path.scope.traverse(path.scope.block, {
+      AssignmentExpression(_path) {
+        if (!types.isIdentifier(_path.node.left, { name: name })) return;
+        _path.remove();
+      },
+    });
+  },
+};
+
+function isEndNode(nodes) {
+  let typeList = ["ReturnStatement", "ThrowStatement"];
+  for (let i = 0; i < nodes.length; i++) {
+    if (typeList.includes(nodes[i].type)) {
+      return true;
+    }
+  }
+  return false;
 }
+
+const ifOptimization = {
+  IfStatement(path) {
+    let { test, consequent, alternate } = path.node;
+    if (!alternate) return;
+    if (isEndNode(consequent.body)) {
+      path.insertAfter(alternate.body);
+      path.node.alternate = null;
+    } else if (isEndNode(alternate.body)) {
+      path.insertAfter(consequent.body);
+      path.replaceInline(
+        types.ifStatement(types.unaryExpression("!", test), alternate, null)
+      );
+    } else if (
+      types.isIfStatement(consequent.body[consequent.body.length - 1]) &&
+      isEndNode(consequent.body[consequent.body.length - 1].consequent.body)
+    ) {
+      if (
+        generator(alternate.body[0]).code ===
+        generator(consequent.body[consequent.body.length - 1].alternate.body[0])
+          .code
+      ) {
+        path.insertAfter(
+          consequent.body[consequent.body.length - 1].alternate.body
+        );
+        consequent.body[consequent.body.length - 1].alternate = null;
+        path.node.alternate = null;
+      }
+    }
+  },
+};
 
 const TernaryToSwitch = {
-    ConditionalExpression(path, {cases, _name}) {
-        let {test, consequent, alternate} = path.node;
-        if (!types.isBinaryExpression(test, {operator: "=="})) return;
-        let {right, left} = test;
-        if (!types.isIdentifier(right) || !types.isNumericLiteral(left)) return;
-        if (!cases[right.name]) {
-            cases[right.name] = [];
-        }
-        let body = [types.ExpressionStatement(types.AssignmentExpression("=", types.Identifier(_name), consequent))]
-        body.push(types.BreakStatement());
-        cases[right.name].push(types.SwitchCase(test.left, body));
+  ConditionalExpression(path, { cases, _name }) {
+    let { test, consequent, alternate } = path.node;
+    if (!types.isBinaryExpression(test, { operator: "==" })) return;
+    let { right, left } = test;
+    if (!types.isIdentifier(right) || !types.isNumericLiteral(left)) return;
+    if (!cases[right.name]) {
+      cases[right.name] = [];
     }
-}
-
+    let body = [
+      types.expressionStatement(
+        types.assignmentExpression("=", types.identifier(_name), consequent)
+      ),
+    ];
+    body.push(types.breakStatement());
+    cases[right.name].push(types.switchCase(test.left, body));
+  },
+};
 const TernaryReturn = {
-    ReturnStatement(path) {
-        let {argument} = path.node;
-        if (!types.isConditionalExpression(argument)) return;
-        if (!types.isBinaryExpression(argument.test, {operator: "=="})) return;
-        let cases = {};
-        let name = argument.test.right.name;
-        if (!name) return;
-        let _name = `${name}${path.node.start}`
-        path.traverse(TernaryToSwitch, {cases: cases, _name: _name})
-        path.insertBefore(types.VariableDeclaration("var", [types.VariableDeclarator(types.Identifier(_name), null)]))
-        path.replaceInline(types.SwitchStatement(types.Identifier(name), cases[name]))
-        path.insertAfter(types.ReturnStatement(types.Identifier(_name)))
-    }
-}
-
-function isJumpCase(node, name) {
-    if (!types.isSwitchCase(node)) return;
-    let {consequent} = node;
-    let _node = consequent[consequent.length - 2];
-    if (types.isIfStatement(_node) || getJump(_node, name) !== undefined) return;
-    return consequent.slice(0, consequent.length - 1)
-}
-
-const MergeIfCases = {
-    SwitchCase(path) {
-        let {test, consequent} = path.node;
-        let index = consequent.length - 2;
-        let node = consequent[index];
-        let {discriminant, cases} = path.parent;
-        let next = getJumpIndex(node, discriminant.name);
-        if (next != -1) return;
-        let _consequent = node.consequent;
-        let _test = node.test;
-        let {alternate} = node;
-        let n1 = getJump(_consequent, discriminant.name)
-        let n2 = getJump(alternate, discriminant.name)
-        n1 = isJumpCase(cases[n1], discriminant.name)
-        n2 = isJumpCase(cases[n2], discriminant.name)
-        if (n1 == undefined || n2 == undefined) return;
-        consequent[index] = types.IfStatement(_test, types.BlockStatement(n1), types.BlockStatement(n2))
-        path.replaceInline(types.SwitchCase(test, consequent))
-    }
-}
-
-const IfTest = {
-    IfStatement(path) {
-        let {test, consequent, alternate} = path.node;
-        if (!types.isSequenceExpression(test)) return;
-        let {expressions} = test;
-        let len = expressions.length;
-        for (let i = 0; i < len - 1; i++) {
-            let item = expressions[i];
-            path.insertBefore(item)
-        }
-        path.get("test").replaceInline(expressions[len - 1])
-    }
-}
+  ReturnStatement(path) {
+    let { argument } = path.node;
+    if (!types.isConditionalExpression(argument)) return;
+    if (!types.isBinaryExpression(argument.test, { operator: "==" })) return;
+    let cases = {};
+    let name = argument.test.right.name;
+    if (!name) return;
+    let _name = `${name}${path.node.start}`;
+    path.traverse(TernaryToSwitch, { cases: cases, _name: _name });
+    path.insertBefore(
+      types.variableDeclaration("var", [
+        types.variableDeclarator(types.identifier(_name), null),
+      ])
+    );
+    path.replaceInline(
+      types.switchStatement(types.identifier(name), cases[name])
+    );
+    path.insertAfter(types.returnStatement(types.identifier(_name)));
+  },
+};
 
 function SequenceOfStatement(path) {
 	let { scope, parentPath, node } = path;
@@ -375,19 +381,15 @@ const resolveSequence =
 	}
 }
 
-//调用插件，处理源代码
 traverse(ast, IfToSwitch)
 traverse(ast, RestoreJump);
-// 控制流合并
-traverse(ast, MergeJump);
-traverse(ast, MergeDeadCycle);
-traverse(ast, MergeJump);
+traverse(ast, MergeCases);
+traverse(ast, CleaningUpGarbage);
+traverse(ast, ifOptimization);
 traverse(ast, TernaryReturn);
-traverse(ast,resolveSequence);
-traverse(ast, IfTest);
-traverse(ast, BlockSyntax);
+traverse(ast, resolveSequence);
 
-//生成新的js code，并保存到文件中输出
+
 let {code} = generator(ast, opts = {jsescOption: {"minimal": true}});
 
 fs.writeFile(decodeFile, code, (err) => {
